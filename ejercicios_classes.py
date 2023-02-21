de793db5808ae7784ebd3d2bc0ca32db836dd5c1
Gerbert Michell <gmym1987@gmail.com>
2023-02-20 23:04:26 -0500

    Ejercicios classes

---
+++ ejercicios_classes.py
@@ -0,0 +1,26 @@
+# crear una classe operaciones con sus respectivos metodos
+# (sumar, restar, multiplicar y dividir). Esta clase recibira 2 numeros
+
+class OperacionesMatematicas:
+    def __init__(self, valor_1, valor_2):
+        self.a = valor_1
+        self.b = valor_2
+
+    def sumar(self):
+        return self.a + self.b
+
+    def restar(self):
+        return self.a - self.b
+
+    def multiplicar(self):
+        return self.a * self.b
+
+    def dividir(self):
+        return self.__redondear(self.a / self.b)
+
+    def __redondear(self, numero):
+        return round(numero, 2)
+
+operaciones = OperacionesMatematicas(5, 3)
+
+print(operaciones.dividir())
