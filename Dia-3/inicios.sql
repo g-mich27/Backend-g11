912f0b8a097ba8537f6189f5438d65e8e52559e8
Gerbert Michell <gmym1987@gmail.com>
2023-02-25 20:17:38 -0500

    postgresql

--- Dia-3/inicios.sql
+++ Dia-3/inicios.sql
@@ -1,99 +1,104 @@
---Para crear un base de datos
+-- Para crear una base de datos
 CREATE DATABASE pruebas;
-\ l
+-- Lista las bases de datos en el servidor
+-- Comandos para PSQL (https://www.postgresql.org/docs/current/app-psql.html)
+\l
 
-\ c
-CREATE TABLE alumnos (
-id SERIAL NOT NULL PRIMARY KEY,
-nombre TEXT NOT NULL,
-apellido VARCHAR(50),
+-- Nos cambia a la base de datos que queremos
+\c pruebas
 
+
+-- Se crea un enumerable que sirve para dar unas determinadas opciones
 CREATE TYPE tipo_sexo AS ENUM('MASCULINO', 'FEMENINO', 'PANSEXUAL', 'DONUTSEXUAL', 'OTRES');
 
+-- Se crea una tabla en la base de datos
 CREATE TABLE alumnos (
 id SERIAL NOT NULL PRIMARY KEY,
 nombre TEXT NOT NULL,
 apellido VARCHAR(50),
-sexo tipo_sexo DEFAULT 'OTRES',
-fecha_creacion TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+sexo tipo_sexo DEFAULT 'OTRES', -- Si utilizamos un ENUM se puede utilizar un valor por defecto dentro de los valores del ENUM, caso contrario lanzara un error
+fecha_creacion TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP, -- Es la fecha y hora actual del servidor
 matriculado BOOLEAN DEFAULT FALSE
 );
 
---Mostrar todas las tablas de la base de datos
-\dt 
 
---Mostrar el detalle de la tabla (Columnas tipo de datos e informacion de las columnas)
+-- Mostrar todas las tablas de la base de datos
+\dt
+
+-- Mostrar el detalle de la tabla (columnas tipo de datos e informacion de las columnas)
 \d alumnos
 
--- Mostrar todas las tablas y sus indices(mostrara la llave primaria de la tabla que es un indice)
+-- Mostrar todas las tablas y sus indices (mostrara la llave primaria de la tabla que es un indice)
 \d 
 
 -- Para visualizar los valores de un enum
 SELECT enum_range(NULL::tipo_sexo)
 
--- Insertar utilizando solamente columnas que no tengan valores por defecto
+-- Inserta un registro a la base de datos utilizando los valores por defecto
+INSERT INTO alumnos (id, nombre, apellido, sexo, fecha_creacion, matriculado) VALUES
+                     (DEFAULT, 'Eduardo', 'de Rivero', 'MASCULINO', DEFAULT, true);
+
+
+-- Inserta utilizando solamente columnas que no tengan valores por defecto
 INSERT INTO alumnos (nombre, apellido) VALUES
-                      ('Victor', 'Mayta');
+                     ('Victor', 'Mayta');
 
 -- Insertar varios registros
 INSERT INTO alumnos (nombre, apellido) VALUES
-                       ('Juana', 'Martinez'),
-                       ('Robert', 'Juarez'),
-                       ('Mariagracia', 'Quispe');
+                     ('Juana', 'Martinez'),
+                     ('Robert', 'Juarez'),
+                     ('Mariagracia', 'Quispe');
 
 INSERT INTO alumnos (nombre, apellido, sexo, matriculado) VALUES
-                        ('Johana', 'Zuñiga', 'FEMENINO', false),
-                        ('Martin', 'Zea', 'PANSEXUAL', false),
-                        ('Roxana', 'Cutipa', 'DONUTSEXUAL', true);
+                     ('Johana', 'Zuniga', 'FEMENINO', false),
+                     ('Martin', 'Zea', 'PANSEXUAL', false),
+                     ('Roxana', 'Cutipo', 'DONUTSEXUAL', true);
 
 
-SELECT * FROM alumnos;
-
+-- SELECCIONA todas las columnas de los registros cuyo sexo sea MASCULINO
 SELECT * FROM alumnos WHERE sexo='MASCULINO';
 
-SELECT * FROM alumnos WHERE sexo='MASCULINO' OR matriculado=false;
+-- SELECCIONA todos los alumos cuyo sexo sea masculino y la matricula sea verdadera
+SELECT * FROM alumnos WHERE sexo='MASCULINO' AND matriculado=true;
 
-SELECT * FROM alumnos WHERE nombre LIKE 'o';
 
-SELECT * FROM alumnos WHERE nombre LIKE '%o';
-
+-- SELECIONA todos los alumnos que contenga la letra o en cualquier parte del nombre
 SELECT * FROM alumnos WHERE nombre LIKE '%o%';
 
-SELECT * FROM alumnos WHERE nombre LIKE '_o%';
-
+-- SELECCION todos los alumnos cuyo nombre en la tercera posicion tenga la letra o,
+-- NOTA: el '_' sirve para indicar cuantas posiciones no deseo saber que hay 
 SELECT * FROM alumnos WHERE nombre LIKE '__o%';
 
-SELECT * FROM alumnos WHERE nombre LIKE '%o_';
+-- SELECCIONA todos los alumnos cuyo nombre en la tercera posicion sea la letra 'u' o la letra 'h'
+SELECT * FROM alumnos WHERE nombre LIKE '__u%' OR nombre LIKE '__h%';
 
-SELECT * FROM alumnos WHERE nombre LIKE '%o_%';
-
-SELECT * FROM alumnos WHERE nombre LIKE '__u%' OR nombre like '__h%';
-
+-- SELECCIONA todos los alumnos cuyo nombre tenga la letra 'x' o cuyo sexo sea PANSEXUAL
 SELECT * FROM alumnos WHERE nombre LIKE '%x%' OR sexo = 'PANSEXUAL';
 
---Asi se crea una tabla con relacion  entre la tabla alumnos y su columna id
+
+-- Asi se crea una tabla con relacion entre la tabla alumnos y su columna id
 CREATE TABLE direcciones(
     id SERIAL PRIMARY KEY,
     direccion TEXT,
     numero int,
     referencia TEXT,
-    alumno_id INT,      -- el tipo de dato tiene que el mismo que la otra columna sino dara error
+    alumno_id INT, -- El tipo de dato tiene que ser el mismo que la otra columna sino dara error
     CONSTRAINT fk_alumnos FOREIGN KEY (alumno_id) REFERENCES alumnos(id)
 );
 
---DDl (ALTER)
+-- DDL (ALTER)
 
 -- TAREA
 -- PASOS: Primero ingresar los datos y luego resolver las siguientes queries:
--- DIRECCION, NUM, REFERENCIA, ALUMNO_ID
-'Calle los Girasoles', 750, 'Al costado de la polleria', 1
-'Calle Los aviadores', 1050, NULL,2
+-- DIRECCION            NUM     REFERENCIA              ALUMNO_ID
+'Calle Los Girasoles', 750, 'Al costado de la polleria', 1
+'Calle Los Aviadores', 1050, NULL,2
 'Av El Sol', 125, 'Del ovalo a media cuadra', 1
 'Av Los Gallos', 777, NULL, 3
 'Av Tupac Yupanqui', 123, NULL, 7
-'Av Siempre viva', 7840, 'Al frente de la ferreteria', 8
-'Calle Los martires', 6520, NULL, 5
-'Pasaje de las flores', 526, NULL, 4
+'Av Siempre Viva', 7840, 'Al frente de la ferreteria', 8
+'Calle Los Martires', 6520, NULL, 5
+'Pasaje de las Flores', 526, NULL, 4
 'Alameda Chabuca', 740, 'Dos cuadras de la piscina', 6
 'Callejon Bravo', 14, 'A dos casas de la reja', 3
 
@@ -102,8 +107,7 @@ CREATE TABLE direcciones(
 -- 3. Listar todas las direcciones que sean menores que 1000
 -- 4. Listar todas las direcciones que sean o Av o Pasaje
 -- 5. Listar todas las direcciones de los alumnos 1 o que vivan en calles o que no tengan referencias
--- 6. Listar todas las direcciones que sean calle y que su referencia no   sea nula y que su alumno sea el 1
-
+-- 6. Listar todas las direcciones que sean calle y que su referencia no sea nula y que su alumno sea el 1
 
 -- Para borrar una tabla
 DROP TABLE nombre_tabla
