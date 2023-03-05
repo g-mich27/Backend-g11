a0134ffaf49ece2f4c15bb10fc2996c2398900cf
Gerbert Michell <gmym1987@gmail.com>
2023-03-04 21:20:23 -0500

    creacion del primer modelo y modelado

--- Dia-1/flask-postgres.py
+++ Dia-1/flask-postgres.py
@@ -1,36 +1,98 @@
 from flask import Flask, request
 from psycopg2 import connect
+from flask_cors import CORS
 
 app = Flask(__name__)
+# le estamos diciendo que puede acceder cualquier origen, cualquier metodo y enviar cualquier header
+CORS(app, methods = ['GET', 'POST', 'PUT', 'DELETE'], origins= ['http://localhost:5500', 'http://127.0.0.1:5500'])
 
-@app.route('/', methods= ['GET'])
-def inicial():
-    return {
-        'message': 'Bienvenido a mi API'
-    }
-@app.route('/alumnos', methods= ['GET', 'POST'])
+
+# me conecto a la bd
+conexion = connect(host='localhost', database='pruebas', user='postgres', password='sql2023')
+
+@app.route('/', methods = ['GET'])
+def inicial():...
+#     return {
+#         'message': 'Bienvenido a mi API'
+#     }
+
+@app.route('/alumnos', methods = ['GET', 'POST'])
 def alumnos():
-    if request.method == 'GET':
-        # Me conecto a la red
-        conexion = connect(host='localhost', database='pruebas', user='postgres', password='sql2023')
+    if request.method == 'GET':...
+        # creo un cursor que es el responsable de hacer lecturas y escrituras a la bd
+        # cursor = conexion.cursor()
         
-        # Creo un cursor que es el responsable de hacer lecturas y escrituras a al bd
-        cursor = conexion.cursor()
+        # # ejecuto la query
+        # cursor.execute('SELECT * FROM alumnos;')
 
-        # Extraigo la informacion de la ejecución de la query
-        cursor.execute('SELECT * FROM alumnos;')
+        # # extraigo la informacion de la ejecucion de la query
+        # resultado = cursor.fetchall()
 
-        resultados = cursor.fetchall()
+        # print(resultado)
+        # alumnos_resultado = []
 
-        print(resultados)
-        return {
-            'message': 'Yo soy el GET'
-        }
+        # for alumno in resultado:
+        #     info_alumno = {
+        #         'id': alumno[0],
+        #         'nombre': alumno[1],
+        #         'apellido': alumno[2],
+        #         'sexo': alumno[3],
+        #         'fecha_creacion': alumno[4],
+        #         'matriculado': alumno[5]
+        #     }
+
+        #     alumnos_resultado.append(info_alumno)
+        # return {
+        #     'message': alumnos_resultado
+        # }
+    
     elif request.method == 'POST':
+        data = request.json
+        print(data)
+        cursor = conexion.cursor()
+        cursor.execute("INSERT INTO ALUMNOS (nombre, apellido, matriculado) VALUES (%s, %s, %s)",(
+            data.get('nombre'), data.get('apellido'), data.get('matriculado')
+        ))
+        # Indicamos a la base de datos que guarde los cambios (insert) de manera permanente
+        conexion.commit()
+
         return {
-            'message': 'Yo soy el POST'
+            'message': 'Alumno ingresado exitosamente'
         }
+    
+@app.route('/alumno/<id>', methods = ['GET', 'PUT', 'DELETE'])
+def gestion_alumno(id):
+    if request.method == 'GET':
+
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM alumnos WHERE id =%s",(id,))
+        alumno = cursor.fetchone()
+        # si el alumno no existe indicar en el message que el alumno no existe, caso contrario devolver el alumno
+        if alumno:
+            #significa que el alumno existe
+            return {
+                'content': {
+                'id': alumno[0],
+                'nombre': alumno[1],
+                'apellido': alumno[2],
+                'sexo': alumno[3],
+                'fecha_creacion': alumno[4],
+                'matriculado': alumno[5]
+                }
+            }
+        else:
+            return {
+                'message': 'El alumno no existe'
+            }
+        
+    elif request.method == 'PUT':
+        # TODO: Recibir la informacion del body y el id por la url y modificar la data del alumno, primero validar si el alumno existe, si no existe no hacer ninguna modificacion, si existe hacer la modificacion
+        pass
+
+    elif request.method == 'DELETE':
+        # TODO: Recibir el id por la url y validar si el alumno existe, si existe, eliminarlo (hacer un delete) caso contrario indicar que el alumno no existe
+        pass
 
 if __name__ == '__main__':
-    # debug > indica que cada vez que guardemos un archivo del proyecyo el servidor se reinicie automaticamente
-    app.run(debug=True)
+    # debug > indicar que cada vez que guardemos un archivo del proyecto el servidor se reinicie automaticamente
+    app.run(debug = True)
---
+++ flask-con-orm/app.py
@@ -0,0 +1,35 @@
+# primero se importan las librerias
+from flask import Flask
+from dotenv import load_dotenv
+from os import environ
+# from flask_migrate import Migrate
+# from flask_restful import Api
+# se importan los archivos dentro del proyecto
+from base_de_datos import conexion
+# from models.nivel_model import Nivel
+# from models.maestro_model import Maestro
+# from models.seccion_model import Seccion
+# from controllers.nivel_controller import NivelController, UnNivelController
+
+
+load_dotenv() # es en el encargado de leer el archivo .env si es que existe y agregar las variables en ese archivo como si fueran variables de entorno 
+
+app = Flask(__name__)
+# dialecto://usuario:password@host:puerto/base_de_datos
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+
+# flask_api = Api(app=app)
+
+# si quiero crear mi conexion en otro archivo e inicializar la configuracion de mi conexion tengo que utilizar el metodo init_app y es aca donde le pasare el parametro de mi instancia de la clase Flask
+conexion.init_app(app)
+
+# ahora realizo la inicializacion de mi clase Migrate
+# se le pasa la aplicacion como primer parametro y la conexion (instancia de SQLMigrate)
+# Migrate(app=app, db=conexion)
+
+# defino las rutas de mi API
+# flask_api.add_resource(NivelController, '/nivel')
+# flask_api.add_resource(UnNivelController,'/nivel/<id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
